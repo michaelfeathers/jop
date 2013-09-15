b912b193ac01b2dc77739a21ae9fc89eef44f546
Michael Feathers <mfeathers@acm.org>
2013-09-14 22:13:09 -0400

    refactored to handle numeric prefix arguments explicitly

--- lib/jop.rb
+++ lib/jop.rb
@@ -5,6 +5,15 @@ class Jop
     @command_text = command_text
   end
 
+  def numeric_literal? text
+    text =~ /_?\d+/
+  end
+
+  def to_numeric text
+     return text.to_i if text =~ /^\d+/
+     return -(text[1..text.length-1]).to_i
+  end
+
   def eval_on ary
     return [] if @command_text.size == 0
     elements = []
@@ -26,10 +35,12 @@ class Jop
       ary.zip(0..(ary.count)).sort_by {|e| e[0] }.map {|e| e[1] }.reverse
     else
       elements = @command_text.split
-      times_string = elements[0]
-      times = times_string[0] == '_' ? -times_string[1..(times_string.length-1)].to_i : times_string.to_i
-      return ary.take(times) if times >= 0
-      ary.reverse.take(-times).reverse
+      if numeric_literal?(elements[0])
+        times = to_numeric(elements[0])
+        return ary.take(times) if times >= 0
+        return ary.reverse.take(-times).reverse
+      end
+      []
     end
   end
 end
