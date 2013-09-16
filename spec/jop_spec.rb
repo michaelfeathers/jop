ee0c9daf7a8fcfffd4fa212b23a5a380f9061dfc
Michael Feathers <mfeathers@acm.org>
2013-09-15 21:28:44 -0400

    added pending specs for rotate

--- spec/jop_spec.rb
+++ spec/jop_spec.rb
@@ -87,4 +87,10 @@ describe Jop do
     [0,1,2].j('1 |.').should eq([1, 2, 0])
   end
 
+  it "can rotate an even number of times"
+  it "can rotate an odd number of times"
+  it "can rotate negative"
+
+
+
 end
