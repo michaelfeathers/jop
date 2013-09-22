9bd1a2fecc9be0b320db7309e5c045d70bd14576
Michael Feathers <mfeathers@acm.org>
2013-09-21 20:23:11 -0400

    added case for recognizing a numeric literal

--- lib/tokenizer.rb
+++ lib/tokenizer.rb
@@ -6,5 +6,7 @@ class Tokenizer
 
   def initialize text
     @tokens = []
+    number = text.chars.take_while {|c| c =~ /\d/ }.reduce(:+)
+    @tokens << number if number && number.length > 0
   end
 end
--- spec/tokenizer_spec.rb
+++ spec/tokenizer_spec.rb
@@ -6,4 +6,8 @@ describe Tokenizer do
   it "produces no tokens on an empty string" do
     Tokenizer.new('').tokens.count.should eq(0)
   end
+
+  it "recognizes a number" do
+    Tokenizer.new('42').tokens[0].should eq('42')
+  end
 end
