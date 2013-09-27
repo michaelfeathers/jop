ab223e1129db3a4f489374ad7a33539009454724
Michael Feathers <mfeathers@acm.org>
2013-09-27 14:23:33 -0400

    refactored tokenizer specs toward brevity

--- spec/tokenizer_spec.rb
+++ spec/tokenizer_spec.rb
@@ -3,16 +3,21 @@
 require 'tokenizer'
 
 describe Tokenizer do
-  it "produces no tokens on an empty string" do
-    Tokenizer.new('').tokens.count.should eq(0)
+
+  def tokens text
+    Tokenizer.new(text).tokens
   end
 
-  it "recognizes a number" do
-    Tokenizer.new('42').tokens[0].should eq('42')
+  it 'produces no tokens on an empty string' do
+    tokens('').count.should eq(0)
   end
 
-  it "recognizes count" do
-    Tokenizer.new('#').tokens[0].should eq('#')
+  it 'recognizes a number' do
+    tokens('42')[0].should eq('42')
+  end
+
+  it 'recognizes count' do
+    tokens('#')[0].should eq('#')
   end
 
 end
